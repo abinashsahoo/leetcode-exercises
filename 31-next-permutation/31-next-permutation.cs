92b358bdb8ccdae8a97c7d9a38bfb12820148e4a
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-06-10 00:50:57 -0400

    Time: 183 ms (49.09%), Space: 41.8 MB (85.37%) - LeetHub

--- 31-next-permutation/31-next-permutation.cs
+++ 31-next-permutation/31-next-permutation.cs
@@ -1,3 +1,9 @@
+//1. Find the last peak in the sequence. In case two successive numbers are peak, return the first index
+//2. Pivot is the element just before the the last peak
+//3. If no such pivot exists, we are at the last permutation already. Just reverse the array and done.
+//4. If pivot exists, swap the pivot with right most successor of the pivot.
+//5. Reverse the sub-array starting at last peak index.
+
 public class Solution {
     public void NextPermutation(int[] nums) {
        
@@ -13,6 +19,7 @@ public class Solution {
         Reverse(nums, i);         
     }    
 
+    //Start traversing from the end of the array and find an item which is greater that its left neighbour.
     private int LastPeakIndex(int[] nums)
     {
         int i = nums.Length - 1;
