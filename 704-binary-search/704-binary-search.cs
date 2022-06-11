5f447e4cd1aa2dab81a52313aba19a603d66b696
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-06-11 01:12:42 -0400

    Time: 171 ms (42.16%), Space: 41.6 MB (62.90%) - LeetHub

--- 704-binary-search/704-binary-search.cs
+++ 704-binary-search/704-binary-search.cs
@@ -1,3 +1,11 @@
+//Time complexity : O(logN)
+//Space complexity : O(1)
+
+//https://ai.googleblog.com/2006/06/extra-extra-read-all-about-it-nearly.html
+//middle = (left + right) >> 1;
+//Right shifting binary numbers would divide a number by 2 and left shifting the numbers would multiply it by 2. 
+//middle = (left + right)/2;
+
 public class Solution {
     public int Search(int[] nums, int target) {
         int middle, left = 0;
@@ -5,7 +13,7 @@ public class Solution {
         
         while(left <= right)
         {            
-            middle = (left + right)/2;
+            middle = left + (right - left) / 2; 
             
             if(nums[middle] == target) return middle;
             if(nums[middle] < target) left = middle + 1;
