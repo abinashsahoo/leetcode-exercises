a92088365fb49ecbe3e1f3f683b6db81452d706b
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-06-16 18:37:47 -0400

    Time: 130 ms (31.37%), Space: 37.4 MB (97.47%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cs
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cs
@@ -1,36 +1,16 @@
 public class Solution {
-    public int FindMin(int[] nums) {
-        int left = 1;
-        int right = nums.Length - 1;
-        
-        while(left <= right)
-        {
-            int mid = left + (right - left)/2;            
-            if(nums[mid] < nums[mid - 1])
-            {
-                return nums[mid];
-            }
-            else if (nums[mid] > nums[mid - 1] && nums[mid] > nums[0])
-            {
-                left = mid + 1;
-            }
-            else
-            {
-                right = mid;
-            }
-        }
-        
-        return nums[0];
-    }
-    
 //     public int FindMin(int[] nums) {
-//         int left = 0;
+//         int left = 1;
 //         int right = nums.Length - 1;
         
-//         while(left < right)
-//         {            
+//         while(left <= right)
+//         {
 //             int mid = left + (right - left)/2;            
-//             if (nums[mid] >= nums[0])
+//             if(nums[mid] < nums[mid - 1])
+//             {
+//                 return nums[mid];
+//             }
+//             else if (nums[mid] > nums[mid - 1] && nums[mid] > nums[0])
 //             {
 //                 left = mid + 1;
 //             }
@@ -40,6 +20,26 @@ public class Solution {
 //             }
 //         }
         
-//         return nums[left];
+//         return nums[0];
 //     }
+    
+    public int FindMin(int[] nums) {
+        int left = 0;
+        int right = nums.Length - 1;
+        
+        while(left < right)
+        {            
+            int mid = left + (right - left)/2;            
+            if (nums[mid] < nums[right])
+            {
+                right = mid;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+        
+        return nums[left];
+    }
 }
