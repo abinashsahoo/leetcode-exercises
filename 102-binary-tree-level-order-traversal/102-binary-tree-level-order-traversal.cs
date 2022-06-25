665c464c8299899eed8c65a858bc94c130c96b46
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-06-25 17:27:32 -0400

    Time: 149 ms (91.97%), Space: 43.2 MB (12.59%) - LeetHub

---
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cs
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public IList<IList<int>> LevelOrder(TreeNode root) {        
+        var result = new List<IList<int>>();
+        
+        var queue = new Queue<TreeNode>();
+        if(root !=null) queue.Enqueue(root);
+        
+        while (queue.Count > 0)
+        {
+            int count = queue.Count; 
+            var level = new List<int>();           
+            for(int i = 0; i < count; i++)
+            {
+                var node = queue.Dequeue();
+                level.Add(node.val);
+                
+                if(node.left != null) queue.Enqueue(node.left);
+                if(node.right != null) queue.Enqueue(node.right);                
+            }
+            
+            result.Add(level);
+        }
+        
+        return result;
+    }
+}
