51e72253832f7f980b24fcb38dadfc8c418b4261
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-06-26 20:19:18 -0400

    Time: 86 ms (92.34%), Space: 38 MB (12.91%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cs
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cs
@@ -10,15 +10,39 @@
  * }
  */
 public class Solution {
+    
     public ListNode SwapPairs(ListNode head) {
-        if(head == null || head.next == null) return head;
+        
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        
+        ListNode prevNode = dummy;
+        
+        while(head != null && head.next != null)
+        {        
+            prevNode.next = head.next;
+            head.next = prevNode.next.next;
+            prevNode.next.next = head;
+            
+            prevNode = head;
+            head = head.next;
+        }
+        
+        return dummy.next;
+    }
+    
+    public ListNode SwapPairs1(ListNode head) {
+        if(head == null || head.next == null) 
+        {
+            return head;
+        }
         
         var temp = head.next;
         head.next = temp.next;
         temp.next = head;
         head = temp;
         
-        head.next.next = SwapPairs(head.next.next);
+        head.next.next = SwapPairs1(head.next.next);
         
         return head;
     }
