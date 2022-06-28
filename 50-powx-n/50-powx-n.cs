5db5fe4851f916bf02eea4b4383964ed5b4f61be
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-06-27 23:26:42 -0400

    Time: 35 ms (54.94%), Space: 25.5 MB (54.25%) - LeetHub

---
+++ 50-powx-n/50-powx-n.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    
+    public double MyPow(double x, int n) {
+        long N = n;
+        if (N < 0)
+        {
+            x = 1/x;
+            N = -N;
+        }
+        
+        return FastPow(x, N);
+    }
+    
+    private double FastPow(double x, long n) {
+        if (n == 0)
+        {
+            return 1;
+        }
+        
+        double half = FastPow(x, n/2);
+        if(n % 2 == 0)
+            return half * half;
+        else
+            return half * half * x;
+    }    
+
+}
