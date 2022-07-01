1c84dc4025c6ac4e651af5598436abbf9ecbc40d
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-07-01 15:51:59 -0400

    Time: 145 ms (63.97%), Space: 40.6 MB (67.39%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cs
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cs
@@ -1,8 +1,17 @@
 public class Solution {
     public int MaximumUnits(int[][] boxTypes, int truckSize) {
         int result = 0;
-        var sortedList = boxTypes.OrderByDescending(b => b[1]);
-        foreach (var boxType in sortedList)
+        //Approach 1
+        //var sortedList = boxTypes.OrderByDescending(b => b[1]);
+        
+        //Approach 2
+        //Array.Sort(boxTypes, (a, b) =>  b[1] - a[1]);
+        
+        //Approach 3
+        var comparer = Comparer<int>.Default;
+        Array.Sort(boxTypes, (a, b) =>  comparer.Compare(b[1], a[1]));
+        
+        foreach (var boxType in boxTypes)
         {
             int numOfBoxes = boxType[0];
             int numOfUnits = boxType[1];
