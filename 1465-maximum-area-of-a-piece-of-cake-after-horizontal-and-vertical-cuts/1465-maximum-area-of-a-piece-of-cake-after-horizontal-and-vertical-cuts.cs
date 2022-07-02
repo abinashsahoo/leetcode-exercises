2d7207d5805ff1fab7bcbe34333f28ead55c0c57
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-07-02 01:51:29 -0400

    Time: 274 ms (15.26%), Space: 44.6 MB (96.61%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cs
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cs
@@ -1,5 +1,32 @@
 public class Solution {
-    public int MaxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
+    public int MaxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) 
+    {
+        Array.Sort(horizontalCuts);
+        Array.Sort(verticalCuts);
+        
+        //long
+        long maxHeight = GetMax(h, horizontalCuts);
+        long maxWidth = GetMax(w, verticalCuts);
+
+        const int MOD = (int)1e9 + 7;
+        return (int)(maxHeight * maxWidth % MOD);
+    }
+    
+    int GetMax(int h, int[] cuts) 
+    {
+        Array.Sort(cuts);
+        
+        int n = cuts.Length;
+        int max = Math.Max(cuts[0], h - cuts[n - 1]);
+        for (int i = 0; i < n - 1; i++) 
+        {
+            max = Math.Max(max, cuts[i + 1] - cuts[i]);
+        }
+        return max;
+    }
+    
+    public int MaxArea1(int h, int w, int[] horizontalCuts, int[] verticalCuts) 
+    {
         Array.Sort(horizontalCuts);
         Array.Sort(verticalCuts);
         
