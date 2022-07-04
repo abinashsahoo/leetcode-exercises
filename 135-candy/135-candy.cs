d168a750062ffe6b2f5e38efba298ae5ddcac164
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-07-03 23:26:36 -0400

    Time: 133 ms (72.02%), Space: 42.2 MB (38.34%) - LeetHub

---
+++ 135-candy/135-candy.cs
@@ -0,0 +1,32 @@
+//[1,3,2,2,1]
+//[1,2,87,87,87,2,1]
+public class Solution {
+    public int Candy(int[] ratings) {
+        
+        if (ratings.Length <=1) return 1;
+        
+        int[] candy = new int[ratings.Length];
+        candy[0] = 1;
+        for (int i = 1; i < ratings.Length; i++)
+        {
+            if(ratings[i] > ratings[i - 1])
+            {
+                candy[i] = candy[i - 1] + 1;
+            }
+            else
+            {
+                candy[i] = 1;
+            }
+        }
+        
+        for (int i = candy.Length - 1; i > 0; i--)
+        {
+            if(ratings[i - 1] > ratings[i] && candy[i - 1] <= candy[i])
+            {
+                candy[i - 1] = candy[i] + 1;
+            }
+        }
+        
+        return candy.Sum(c => c);
+    }
+}
