ee1667c71b88147d9ce27051d1c3bf6f338eb467
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-07-04 23:40:15 -0400

    Time: 261 ms (54.56%), Space: 44.6 MB (84.85%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cs
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cs
@@ -1,7 +1,33 @@
 //[0,3,7,2,5,8,4,6,0,1] -> what if equals; When all numbers are part of subsequence
 //[9,1,4,7,3,-1,0,5,8,-1,6]
 public class Solution {
-    public int LongestConsecutive(int[] nums) {
+
+        public int LongestConsecutive(int[] nums) {
+        if(nums.Length <= 1) return nums.Length;
+        
+        var set = new HashSet<int>(nums); //O(1)
+        
+        int longestStreak = 0;
+        int currentStreak = 0;
+        
+        foreach (int num in set)//Index can't be used for HashSet; so use foreach
+        {
+            if(!set.Contains(num - 1)) //If set.Contains(num - 1), num is a part of longer subsequence, skip it for now
+            {
+                currentStreak = 1;
+                int i = 1;
+                while (set.Contains(num + i))
+                {
+                    currentStreak++;
+                    i++;
+                }
+                longestStreak = Math.Max(longestStreak, currentStreak);
+            }
+        }
+        return Math.Max(longestStreak, currentStreak); //important When all numbers are part of subsequence
+    }
+    
+    public int LongestConsecutive1(int[] nums) {
         if(nums.Length <= 1) return nums.Length;
         
         Array.Sort(nums);
@@ -17,12 +43,11 @@ public class Solution {
             }
             else if (nums[i - 1] != nums[i])
             {
-                Console.Write($"{currentStreak}-[{nums[i]}]->");
                 longestStreak = Math.Max(longestStreak, currentStreak);
                 currentStreak = 1;
-                //i++;
+                //i++; //for loop is going to increase it anyway
             }
         }
-        return Math.Max(longestStreak, currentStreak);
+        return Math.Max(longestStreak, currentStreak); //important When all numbers are part of subsequence
     }
 }
