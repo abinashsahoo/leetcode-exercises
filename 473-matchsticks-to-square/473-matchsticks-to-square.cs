466e03e59735ba98c46e1c3dde0c75d1a71f4353
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-07-12 01:54:44 -0400

    Time: 304 ms (62.79%), Space: 39.5 MB (79.07%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.cs
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cs
@@ -1,5 +1,8 @@
 // [5,5,5,5,4] -> Only sum % 4 won't work: matchsticks.Sum() % 4 == 0
 public class Solution {
+    private bool _isSquare = false;
+    private int _side;
+    
     public bool Makesquare(int[] matchsticks) {
         if (matchsticks == null || matchsticks.Length == 0)
         {
@@ -12,43 +15,49 @@ public class Solution {
             return false;
         }
         
-        int side = perimeter/4;
+        _side = perimeter/4;
+        
+        //Extra validation can be added
+        if (matchsticks.Any(num => num > _side))
+        {
+            return false;
+        }
         
         //NOTE: Got TLE without Sort
         //Reverse sort the matchsticks because we want to consider the biggest one first.
-        Array.Sort(matchsticks, (a, b) => b.CompareTo(a));
+       //Array.Sort(matchsticks, (a, b) => b.CompareTo(a));
+        Array.Sort(matchsticks, (a, b) => b - a);
         
         //This array represents the 4 sides and their current lengths
         int[] sums = new int[4];
         
-        return Dfs(0);
+        Dfs(matchsticks, 0, sums);
         
-        bool Dfs(int index)
+        return _isSquare;
+    }
+    
+    private void Dfs(int[] matchsticks, int index, int[] sums)
+    {
+        if (index == matchsticks.Length)
         {
-            if (index == matchsticks.Length)
+            //return sums[0] == sums[1] && sums[1] == sums[2] && sums[2] == sums[3];
+            _isSquare = sums.All(s => s == _side);
+        }
+        
+        if(_isSquare) return;
+
+        int element = matchsticks[index];
+        //Try adding it to each of the 4 sides (if possible)
+        for (int i = 0; i < 4; i++)
+        {
+            //If this matchstick can fit in the space left for the current side
+            if(sums[i] + element <= _side)
             {
-                return sums[0] == sums[1] && sums[1] == sums[2] && sums[2] == sums[3];
+                sums[i] += element;
+                Dfs(matchsticks, index + 1, sums);
+                //Revert the effects of recursion because we no longer need them for other recursions.
+                sums[i] -= element;
             }
-            
-            int element = matchsticks[index];
-            //Try adding it to each of the 4 sides (if possible)
-            for (int i = 0; i < 4; i++)
-            {
-                //If this matchstick can fit in the space left for the current side
-                if(sums[i] + element <= side)
-                {
-                    sums[i] += element;
-                    if(Dfs(index + 1))
-                    {
-                        return true;
-                    }
-                    
-                    //Revert the effects of recursion because we no longer need them for other recursions.
-                    sums[i] -= element;
-                }
-            }
-            
-            return false;
         }
     }
 }
