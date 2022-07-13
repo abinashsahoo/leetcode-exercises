54038c4cf8cdac6e27f3030cead3b10b2801ee05
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-07-13 12:46:25 -0400

    Time: 111 ms (73.74%), Space: 37.4 MB (83.84%) - LeetHub

--- 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.cs
+++ 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.cs
@@ -1,4 +1,27 @@
+//Using  priority_queue: https://leetcode.com/problems/minimum-amount-of-time-to-fill-cups/discuss/2277595/c%2B%2B-or-simple-solution
+
+//It's a brain-teaser.
+//The possible strategy is greedily fill up 2 cups with different types of water.
+//We pick the 2 types with the most number of cups.
+
 public class Solution {
+    //https://leetcode.com/problems/minimum-amount-of-time-to-fill-cups/discuss/2261394/JavaC%2B%2BPython-max(max(A)-(sum(A)-%2B-1)-2)
+    public int FillCups(int[] amount) {
+        int max = 0;
+        int sum = 0;
+        
+        foreach (int a in amount)
+        {
+            max = Math.Max(a, max);
+            sum += a;
+        }        
+              
+        return Math.Max(max, (sum + 1)/2);//taking ceiling of sum/2; because if odd number, we need 1 extra step
+    }
+}
+
+
+public class Solution3 {
     public int FillCups(int[] amount) {
         var sorted = amount.OrderByDescending(a => a).ToArray();
         int result = 0;
@@ -11,8 +34,7 @@ public class Solution {
             Array.Sort(sorted, (a, b) => b - a);
         }
         
-        result += sorted[0];
-        
+        result += sorted[0];        
         return result;
     }
 }
