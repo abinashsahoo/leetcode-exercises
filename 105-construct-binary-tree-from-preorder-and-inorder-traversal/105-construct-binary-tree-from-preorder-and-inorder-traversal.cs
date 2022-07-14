569d9b17e506738a4e47f78ff4c8b4e7e2f30aaa
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-07-14 03:29:18 -0400

    Time: 127 ms (62.67%), Space: 38.8 MB (74.80%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cs
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cs
@@ -17,6 +17,36 @@
 //Unique value??
 
 public class Solution {
+    public TreeNode BuildTree(int[] preorder, int[] inorder) {        
+        return BuildTree(preorder, inorder, 0, 0, inorder.Length - 1);
+    }
+    
+    private TreeNode BuildTree(int[] preorder, int[] inorder, int preorderStart, int inorderStart, int inorderEnd) {
+        if(inorderStart > inorderEnd)
+        {
+            return null;
+        }
+                
+        int root = preorder[preorderStart];
+        var node = new TreeNode { val = root };
+        
+        int rootIndex = -1;
+        for (int i = inorderStart; i <= inorderEnd; i++)
+        {
+            if (inorder[i] == root)
+            {
+                rootIndex = i;
+            }
+        }
+        
+        node.left = BuildTree(preorder, inorder, preorderStart + 1, inorderStart, rootIndex - 1);
+        node.right = BuildTree(preorder, inorder, preorderStart + rootIndex - inorderStart + 1, rootIndex + 1, inorderEnd);
+        
+        return node;
+    }
+}
+
+public class Solution1 {
     int preorderRootIndex = 0;    
     public TreeNode BuildTree(int[] preorder, int[] inorder) {
         if(inorder == null || inorder.Length == 0)
@@ -52,7 +82,7 @@ public class Solution {
         }
         
         if (left.Count > 0)
-            ++preorderRootIndex;
+            ++preorderRootIndex; // Passing it a param did not work because this has to linearly increase while left calling left recursively and pass the increased value to right recursives
         node.left = BuildTree(preorder, left.ToArray());
         if (right.Count > 0)
             ++preorderRootIndex;
