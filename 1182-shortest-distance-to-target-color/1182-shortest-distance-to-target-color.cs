2f786f6084c72cdfabcd211d0d08f5ba0c1ab472
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-07-15 00:57:48 -0400

    Time: 719 ms (79.41%), Space: 58.2 MB (5.88%) - LeetHub

--- 1182-shortest-distance-to-target-color/1182-shortest-distance-to-target-color.cs
+++ 1182-shortest-distance-to-target-color/1182-shortest-distance-to-target-color.cs
@@ -36,20 +36,45 @@ public class Solution {
         return result;
     }
     
-    private int SearchColor(List<int> sortedList, int i)
+    private int SearchColor(IList<int> sortedList, int i)
     {
         if (i < sortedList[0]) 
             return sortedList[0] - i;
         if (i > sortedList[^1]) 
             return i - sortedList[^1];
         
-        int result = sortedList.BinarySearch(i);
-        if(result < 0)
-        {
-            result = ~result;
-            return Math.Min(sortedList[result] - i, i - sortedList[result - 1]);
-        }
+        int result = BinarySearch(sortedList, i);
+        return Math.Min(sortedList[result] - i, i - sortedList[result - 1]);
         
-        return -1;
+        // int result = sortedList.BinarySearch(i);
+        // if(result < 0)
+        // {
+        //     result = ~result;
+        //     return Math.Min(sortedList[result] - i, i - sortedList[result - 1]);
+        // }
+        
+        //return -1;
+    }
+    
+    //Returns insert point (if within array) or exact match
+    private int BinarySearch(IList<int> list, int target)
+    {
+        int left = 0;
+        int right = list.Count - 1;
+        
+        while (left < right)
+        {
+            int mid = left + (right - left)/2;
+            
+            if (target < list[mid])
+            {
+                right = mid;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }        
+        return left;
     }
 }
