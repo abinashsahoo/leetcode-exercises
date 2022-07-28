4fb59b32f03f9a293fbe1cd39725a2a7715ed766
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-07-28 12:45:41 -0400

    Time: 1692 ms (11.28%), Space: 38.1 MB (26.69%) - LeetHub

---
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cs
@@ -0,0 +1,82 @@
+public class Solution {
+    public string LongestPalindrome(string s) {
+
+        string result = string.Empty;
+        for (int left = 0; left < s.Length; left++)
+        {
+            int right = left;//it should start at "left"
+            while (right < s.Length)
+            {
+                if (IsPalindrome(s, left, right) && result.Length < right - left + 1)
+                {                
+                    result = s.Substring(left, right - left + 1);                    
+                }
+                right++;
+            }            
+        }
+        return result;
+    }
+    
+    private bool IsPalindrome(string s, int substringStart, int substringEnd)
+    {
+        //Console.WriteLine(s.Substring(substringStart, substringEnd - substringStart + 1));
+        
+        int start = substringStart;
+        int end = substringEnd;
+        
+        while (start < end)
+        {
+            if (s[start] != s[end])
+            {
+                return false;                
+            }
+            
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
+
+// Incorrect: "ba" is not palindrome, but "bab" is. So we can't slide the window. We have to test all substrings
+public class Solution1 {
+    public string LongestPalindrome(string s) {
+        int left = 0;
+        int right = 0;
+        string result = string.Empty;
+        while (right < s.Length)
+        {
+            if (IsPalindrome(s, left, right) && result.Length < right - left + 1)
+            {                
+                result = s.Substring(left, right - left + 1);
+                right++;
+            }
+            else
+            {
+                left++;
+                right++;
+            }
+        }
+        return result;
+    }
+    
+    private bool IsPalindrome(string s, int substringStart, int substringEnd)
+    {
+        Console.WriteLine(s.Substring(substringStart, substringEnd - substringStart + 1));
+        
+        int start = substringStart;
+        int end = substringEnd;
+        
+        while (start < end)
+        {
+            if (s[start] != s[end])
+            {
+                return false;                
+            }
+            
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
