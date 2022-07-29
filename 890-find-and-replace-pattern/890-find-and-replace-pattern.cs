d1955ba32b10307467ab28875ee1f20ce080b7d0
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-07-29 00:21:09 -0400

    Time: 194 ms (64.00%), Space: 42.7 MB (94.00%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.cs
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cs
@@ -1,5 +1,42 @@
-//Test case: ["ef","fq","ao","at","lx"], pattern = "ya"
 public class Solution {
+    public IList<string> FindAndReplacePattern(string[] words, string pattern) {
+        var result = new List<string>();
+        foreach (var word in words)
+        {
+            if (IsMatch(word, pattern))
+            {
+               result.Add(word);
+            }
+        }
+        return result;
+    }
+    
+    private bool IsMatch(string word, string pattern)
+    {
+        var dict = new Dictionary<char, char>();
+        
+        for (int i = 0; i < pattern.Length; i++)
+        {
+            char p = pattern[i];
+            char w = word[i];
+
+            if(!dict.ContainsKey(p))
+            {
+                if(dict.ContainsValue(w)) //This is O(n) same as dict.Values.Count()
+                    return false;
+                dict[p] = w;
+            }
+            else if (dict[p] != w)
+            {
+                return false;
+            }
+        }        
+        return true;
+    }
+}
+
+//Test case: ["ef","fq","ao","at","lx"], pattern = "ya"
+public class Solution3 {
     public IList<string> FindAndReplacePattern(string[] words, string pattern) {
         var result = new List<string>();
         foreach (var word in words)
@@ -31,7 +68,8 @@ public class Solution {
             }
         }
         
-        if(dict.Values.GroupBy(v => v).Any(v => v.Count() > 1)) //If Any duplicate map?
+        //if(dict.Values.GroupBy(v => v).Any(v => v.Count() > 1)) //If Any duplicate map?
+        if(dict.Values.Distinct().Count() != pattern.Distinct().Count())
             return false;
         else
             return true;
