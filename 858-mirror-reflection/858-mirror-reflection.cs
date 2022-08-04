7ea3e6fb38cb227639f56c09791e6c7850150525
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-08-03 23:50:13 -0400

    Time: 43 ms (55.56%), Space: 25.4 MB (11.11%) - LeetHub

--- 858-mirror-reflection/858-mirror-reflection.cs
+++ 858-mirror-reflection/858-mirror-reflection.cs
@@ -8,8 +8,26 @@ OR, (max m could be q, max n could be p to satisfy: m*p = n*q)
 p = odd, q = even: return 0
 p = odd, q = odd: return 1
 p = even, q = odd: return 2
+
+So, this problem can be transformed into finding m * p = n * q, where
+m = the number of room extension + 1.
+n = the number of light reflection + 1.
 */
 public class Solution {
+    public int MirrorReflection(int p, int q) {
+        int m = q, n = p;
+        while(m % 2 == 0 && n % 2 == 0){
+            m /= 2;
+            n /= 2;
+        }
+        if (m % 2 == 0 && n % 2 == 1) return 0;
+        if (m % 2 == 1 && n % 2 == 1) return 1;
+        if (m % 2 == 1 && n % 2 == 0) return 2;
+        return -1;
+    }
+}
+
+public class Solution1 {
     public int MirrorReflection(int p, int q) {
         if (q == 0) return 0;
     
