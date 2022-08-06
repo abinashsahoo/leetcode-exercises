5e0971b7658837bace05dccecf4dbd79780f7d8f
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-08-06 17:55:07 -0400

    Time: 20 ms (100.00%), Space: 25.4 MB (80.00%) - LeetHub

---
+++ 458-poor-pigs/458-poor-pigs.cs
@@ -0,0 +1,6 @@
+public class Solution {
+    public int PoorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        int states = minutesToTest/minutesToDie + 1;
+        return (int)Math.Ceiling(Math.Log(buckets)/Math.Log(states));
+    }
+}
