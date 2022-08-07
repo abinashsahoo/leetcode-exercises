ad4a4ea43a7197a9d03f7ee91229a6afbb6b5d0c
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-08-07 18:27:05 -0400

    Time: 30 ms (82.93%), Space: 27.3 MB (60.98%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cs
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cs
@@ -1,4 +1,38 @@
-public class Solution { 
+public class Solution 
+{
+     /*
+        a = 0,
+        e = 1,
+        i = 2,
+        o = 3,
+        u = 4
+    */
+    public int CountVowelPermutation(int n) 
+    {
+        long mod = 1_000_000_007;
+        var count = new long[5]{1, 1, 1, 1, 1}; 
+        for(int i = 2; i <= n; i++)
+        {
+            var countTemp = new long[5];
+            countTemp[0] = count[1] + count[2] + count[4];
+            countTemp[1] = (count[0] + count[2]) % mod;
+            countTemp[2] = (count[1] + count[3]) % mod;
+            countTemp[3] = count[2];
+            countTemp[4] = (count[2] + count[3]) % mod;
+            count = countTemp;
+            // countTemp[0] = count[1];
+            // countTemp[1] = (count[0] + count[2]) % mod;
+            // countTemp[2] = (count[0] + count[1] + count[3] + count[4]) % mod;
+            // countTemp[3] = (count[2] + count[4]) % mod;
+            // countTemp[4] = count[0];
+            // count = countTemp;
+        }
+        
+        return (int) (count.Sum() % mod);
+    }
+}
+
+public class Solution1 { 
     public int CountVowelPermutation(int n) {
         
         const int MOD = (int)1e9 + 7;
