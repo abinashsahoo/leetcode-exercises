dd8be7204e783661274fb61fbfe724a28aa2efe6
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-08-09 10:52:34 -0400

    Time: 128 ms (42.97%), Space: 38.6 MB (74.25%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cs
+++ 125-valid-palindrome/125-valid-palindrome.cs
@@ -24,5 +24,6 @@ public class Solution {
         return true;
     }
     
+    //Could not include  c >= 'A' && c <= 'Z' as well, because z == Z (case insensitive equality)
     private bool IsValidChar(char c) => c >= '0' && c <= '9' || c >= 'a' && c <= 'z';
 }
