1f082807c2acd90e652df78d03e20c6c8b07cb05
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-08-08 23:29:38 -0400

    Time: 246 ms (67.65%), Space: 46.8 MB (66.96%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cs
+++ 217-contains-duplicate/217-contains-duplicate.cs
@@ -1,11 +1,11 @@
 // Questions: Can we modify (sort) the input list?
 public class Solution {
     public bool ContainsDuplicate(int[] nums) {
-        Array.Sort(nums);
+        Array.Sort(nums); //If Quicksort  is used, space complexity would be O(logN)
         int? prevNumber = null;
         for (int i = 0; i < nums.Length; i++)
         {
-            if (prevNumber == nums[i]) // Nullable int quelity with int
+            if (prevNumber == nums[i]) // Nullable int equality with int
             {
                 return true;                
             }
