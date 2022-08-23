869e348e30010eb1d401fd9de30e64fdf6923787
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-08-22 23:40:32 -0400

    Time: 352 ms (67.80%), Space: 57.6 MB (16.27%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cs
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cs
@@ -9,7 +9,67 @@
  *     }
  * }
  */
-public class Solution {
+public class Solution {    
+    public bool IsPalindrome(ListNode head) 
+    {
+        if (head == null) return true;
+        
+        ListNode firstHalfEnd = GetFirstHalfEnd(head);
+        ListNode secondHalfStart = ReverseList(firstHalfEnd.next);
+        
+        ListNode p1 = head;
+        ListNode p2 = secondHalfStart;
+        
+        // Check palindrome
+        bool result = true;
+        while (p2 != null)
+        {
+            if (p1.val != p2.val)
+            {
+                result = false;
+                break;
+            }
+            
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        
+        // Restore the list
+        firstHalfEnd.next = ReverseList(secondHalfStart);        
+        return result;
+    }
+    
+    private ListNode GetFirstHalfEnd(ListNode head)
+    {
+        ListNode fast = head;
+        ListNode slow = head;
+        
+        //while (fast.next != null && fast.next.next != null)
+        while (fast.next?.next != null)
+        {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+    
+    private ListNode ReverseList(ListNode head)
+    {        
+        ListNode prev = null;
+        ListNode curr = head;
+        
+        while (curr != null)
+        {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;        
+    }
+}
+
+public class Solution1 {
     
     private ListNode frontPointer;
     
