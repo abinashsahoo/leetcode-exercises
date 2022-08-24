0b3c51c0f8b63033841def9e1611f5fc02c8cde7
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-08-24 19:56:14 -0400

    Time: 169 ms (5.15%), Space: 33.9 MB (6.96%) - LeetHub

--- 326-power-of-three/326-power-of-three.cs
+++ 326-power-of-three/326-power-of-three.cs
@@ -5,6 +5,8 @@ public class Solution {
         //Math.Log -> base e
         //Math.Log10 -> base 10
         //return (Math.Log10(n) / Math.Log10(3)) % 1 == 0;
+        //NOTE: Epsilon Did not work in c#?
+        Console.WriteLine(double.Epsilon);
          Console.WriteLine((Math.Log10(n) / Math.Log10(3)));
         Console.WriteLine(((Math.Log(n) / Math.Log(3))));
         return ((Math.Log(n) / Math.Log(3)) + 0.00000000000001d) % 1 <= 2 * 0.00000000000001d;
