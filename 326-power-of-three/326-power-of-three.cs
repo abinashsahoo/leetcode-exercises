6bfac16e7184ce344f3b3fc04bc6141bfa0f6d04
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-08-24 19:53:37 -0400

    Time: 195 ms (5.15%), Space: 30.5 MB (6.96%) - LeetHub

--- 326-power-of-three/326-power-of-three.cs
+++ 326-power-of-three/326-power-of-three.cs
@@ -1,8 +1,12 @@
+//129140163
 public class Solution {
     public bool IsPowerOfThree(int n) {
         //check if a number is an integer by taking the decimal part (using % 1)
         //Math.Log -> base e
         //Math.Log10 -> base 10
-        return (Math.Log10(n) / Math.Log10(3)) % 1 == 0;
+        //return (Math.Log10(n) / Math.Log10(3)) % 1 == 0;
+         Console.WriteLine((Math.Log10(n) / Math.Log10(3)));
+        Console.WriteLine(((Math.Log(n) / Math.Log(3))));
+        return ((Math.Log(n) / Math.Log(3)) + 0.00000000000001d) % 1 <= 2 * 0.00000000000001d;
     }
 }
