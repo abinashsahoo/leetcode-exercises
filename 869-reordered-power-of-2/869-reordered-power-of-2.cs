3f27e55dfb5f3e848694495de52881241744a271
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-08-26 13:53:37 -0400

    Time: 76 ms (33.33%), Space: 29.8 MB (33.33%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cs
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cs
@@ -2,7 +2,10 @@ public class Solution {
     public bool ReorderedPowerOf2(int n) {
         var num = GetSortedDigits(n).ToArray();
         
-        for (int i = 0; i < 30; i++)
+        //n <= 10^9, we only need to check powers in the range [0,29]
+        
+        //for (int i = 0; i < 30; i++)
+        for (int i = 0; i <= (int)Math.Log2(1e9); i++)
         {
             var powerOfTwo = GetSortedDigits(1 << i);            
             if(powerOfTwo.SequenceEqual(num))
