e69158ada1f0357bb9557fd473b9fd01f3ae39a5
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-08-29 19:31:29 -0400

    Time: 137 ms (84.52%), Space: 44.9 MB (35.36%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cs
+++ 200-number-of-islands/200-number-of-islands.cs
@@ -48,6 +48,46 @@ public class Solution {
                     Queue<int[]> queue = new Queue<int[]>();
                     
                     queue.Enqueue(new int[] { i, j });
+                    grid[i][j] = '0';
+                    
+                    while (queue.Count > 0)
+                    {
+                        int[] cur = queue.Dequeue();
+                        
+                        for (int k = 0; k < 4; k++)
+                        {
+                            int newX = cur[0] + dx[k],
+                                newY = cur[1] + dy[k];
+                            
+                            if (newX > -1 && newX < grid.Length && newY > -1 && newY < grid[0].Length && grid[newX][newY] != '0')
+                            {
+                                queue.Enqueue(new int[] { newX, newY });
+                                grid[newX][newY] = '0';
+                            }
+                        }
+                    }
+                }
+        
+        return result;
+    }
+}
+
+//BFS -- Marks Visited as 'N'
+public class Solution0 {
+    public int NumIslands(char[][] grid) {
+        int result = 0;
+        int[] dx = new int[] { 1, -1, 0, 0 },
+              dy = new int[] { 0, 0, 1, -1 };
+        
+        for (int i = 0; i < grid.Length; i++)
+            for (int j = 0; j < grid[0].Length; j++)
+                if (grid[i][j] == '1')
+                {
+                    result++;
+                    
+                    Queue<int[]> queue = new Queue<int[]>();
+                    
+                    queue.Enqueue(new int[] { i, j });
                     grid[i][j] = 'N';
                     
                     while (queue.Count > 0)
