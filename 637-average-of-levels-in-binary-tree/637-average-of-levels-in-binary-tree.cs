5965209cdd59b0c501f0fed460ac707d26accf02
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-09-02 15:53:20 -0400

    Time: 307 ms (5.85%), Space: 43.9 MB (95.95%) - LeetHub

--- 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.cs
+++ 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.cs
@@ -11,7 +11,45 @@
  *     }
  * }
  */
+
+//Using DFS, Recursive
 public class Solution {
+    public IList<double> AverageOfLevels(TreeNode root) {
+        var result = new List<double>();
+        if (root == null) return result;
+        
+        List<int> count = new();
+        Average(root, 0, result, count);
+        
+        for (int i = 0; i < result.Count; i++)
+        {
+            result[i] = result[i]/count[i];
+        }
+        return result;
+    }
+    
+    private void Average(TreeNode root, int level, List<double> sum, List<int> count)
+    {
+        if (root == null)
+            return;
+        
+        if (level == sum.Count)
+        {
+            sum.Add(root.val);
+            count.Add(1);
+        }
+        else
+        {
+            sum[level] += root.val;
+            count[level]++;
+        }
+      
+        Average(root.right, level + 1, sum, count);
+        Average(root.left, level + 1, sum, count);
+    }
+}
+
+public class Solution1 {
     public IList<double> AverageOfLevels(TreeNode root) {
         var result = new List<double>();
         if (root == null) return result;
