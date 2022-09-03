b5e7825cbc5c8f90f707a140c700c5676f2789ca
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-09-03 14:02:36 -0400

    Time: 196 ms (6.45%), Space: 35.1 MB (96.77%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cs
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cs
@@ -1,4 +1,36 @@
+//Started from MSD; Compare with my own soln
 public class Solution {
+    public int[] NumsSameConsecDiff(int n, int k) {
+        var result = new List<int>();
+        for (int i = 1; i<=9; i++)
+        {
+            Dfs(n, k, i, result);
+        }
+        return result.ToArray();
+    }
+    
+    private void Dfs(int n, int k, int num, List<int> result)
+    {
+        if (n == 1)//No need of digitPosition, n can be used for that
+        {
+            result.Add(num);
+            return;
+        }
+        
+        int lsd = num % 10;
+        int nextDigit1 = lsd - k;
+        int nextDigit2 = lsd + k;
+        
+        if (nextDigit1 >= 0)//(digit - k >= 0)
+            Dfs(n - 1, k, num * 10 + nextDigit1, result);
+        
+        if (nextDigit2 <=9 && nextDigit1 != nextDigit2)//Removes duplicated //(digit + k <= 9)
+            Dfs(n - 1, k, num * 10 + nextDigit2, result);
+    }
+}
+
+// Started from LSD
+public class Solution1 {
     public int[] NumsSameConsecDiff(int n, int k) {
         var result = new HashSet<int>();
         for (int i = 0; i<=9; i++)
