0dc89115e3d0167a286d9ff3aad2a17ed9dcaf76
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-09-07 19:01:47 -0400

    Time: 190 ms (48.84%), Space: 41 MB (80.23%) - LeetHub

---
+++ 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.cs
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public string Tree2str(TreeNode root) {
+        StringBuilder sb = new();
+        Dfs(root, sb);
+        return sb.ToString();
+    }
+    
+    private void Dfs(TreeNode root, StringBuilder sb)
+    {
+        if (root == null)
+            return;
+        sb.Append(root.val);
+        if (root.left != null && root.right != null)
+        {
+            sb.Append("(");
+            Dfs(root.left, sb);
+            sb.Append(")");
+            
+            sb.Append("(");
+            Dfs(root.right, sb);
+            sb.Append(")");
+        }
+        else if (root.left != null && root.right == null)
+        {
+            sb.Append("(");
+            Dfs(root.left, sb);
+            sb.Append(")");
+        }
+        else if (root.left == null && root.right != null)
+        {
+            sb.Append("(");
+            sb.Append(")");
+            
+            sb.Append("(");
+            Dfs(root.right, sb);
+            sb.Append(")");
+        }
+    }
+}
