3a908c7f4b69c52495b1c3d48eb11e2eb49a027b
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-09-10 11:55:45 -0400

    Time: 613 ms (5.02%), Space: 47.2 MB (36.25%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cs
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cs
@@ -11,6 +11,40 @@ public class Solution {
     }
 }
 
+public class Solution2 {
+    public int MaxProfit(int[] prices) {
+        
+        // if (prices.Length < 2)
+        // {
+        //     throw new ArgumentException("Getting a profit requires at least 2 prices", nameof(prices));
+        // }
+        
+        int lowest = prices[0];
+        int maxProfit = 0;//prices[1] - prices[0]; if we want to retutn loss when price never increases
+        for (int i = 1; i < prices.Length; i++)
+        {
+            maxProfit = Math.Max(maxProfit, prices[i] - lowest);//This calculation has to happen first
+            lowest = Math.Min(lowest, prices[i]);//This one after profit calc; because Minprice should come first
+        }
+        
+        return maxProfit;
+    }
+    
+    //Brute force O(n^2)
+    public int MaxProfit0(int[] prices) {
+        
+        int maxProfit = 0;
+        for (int i = 0; i < prices.Length - 1; i++)
+        {
+            for (int j = i + 1; j < prices.Length; j++)
+            {
+                maxProfit = Math.Max(maxProfit, prices[j] - prices[i]);
+            }
+        }
+        return maxProfit;
+    }
+}
+
 //Throught to use 2 pointers min from left and max from right. But where to stop??
 public class Solution1 {
     public int MaxProfit(int[] prices) {
