d38299ba7ddba2d25ef681ca1f0b25344ed594b9
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-09-10 19:09:57 -0400

    Time: 135 ms (46.97%), Space: 37.9 MB (71.24%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cs
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cs
@@ -2,16 +2,9 @@
 // Can we buy and sell on the same day not to incur loss? - Yes
 // So profit can't go negative
 
-// Greedy?
+// Greedy Solution
 // Check out: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/discuss/2556523/Faster-than-98C%2B%2BGreedyVery-Easy-to-understand
-class Solution {
-
-    public int MaxProfit(int[] prices) {
-        var dp = new int[2] {0, Int32.MinValue};
-        foreach (int price in prices)
-            dp = new int[2] {Math.Max(dp[0], dp[1] + price), Math.Max(dp[1], dp[0] - price)};
-        return dp[0];
-    }
+class SolutionGreedy {
     
     public int MaxProfit1(int[] prices) {
         int minPrice = int.MaxValue, profit = 0;
@@ -37,10 +30,58 @@ class Solution {
     }
 }
 
+public class Solution {
+    
+    public int MaxProfit(int[] prices) {
+        var dpAhead = new int[2];
+        dpAhead[0] = 0; //Not Buy state; stores the max profit
+        dpAhead[1] = 0; //Buy state; stores the max profit
+        var dpCurrent = new int[2];
+        for (int i = prices.Length - 1; i >= 0; i--)
+        {
+            // if (buy == 1) //allowed to buy
+            // {
+            //     profit = Math.Max(-prices[index] + MaxProfit(prices, index + 1, 0, dp), 0 + MaxProfit(prices, index + 1, 1, dp));
+            // }
+            // else //allowed to sell
+            // {
+            //     profit = Math.Max(prices[i] + MaxProfit(prices, index + 1, 1, dp), 0 + MaxProfit(prices, index + 1, 0, dp));
+            // }
+            dpCurrent[0] = Math.Max(dpAhead[0], dpAhead[1] + prices[i]); 
+            dpCurrent[1] = Math.Max(dpAhead[1], dpAhead[0] - prices[i]);
+            
+            dpAhead = dpCurrent;
+        }
+            
+        return dpCurrent[1];
+    }
+    
+    public int MaxProfit1(int[] prices) {
+        var dp = new int[2];
+        dp[0] = 0; //Not Buy state; stores the max profit
+        dp[1] = Int32.MinValue; //Buy state; stores the max profit
+        foreach (int price in prices)
+        {
+            dp[0] = Math.Max(dp[0], dp[1] + price); 
+            dp[1] = Math.Max(dp[1], dp[0] - price);
+        }
+            
+        return dp[0];
+    }
+    
+    // public int MaxProfit(int[] prices) {
+    //     var dp = new int[2]{0, Int32.MinValue};
+    //     foreach (int price in prices)
+    //         dp = new int[2] {Math.Max(dp[0], dp[1] + price), Math.Max(dp[1], dp[0] - price)};
+    //     return dp[0];
+    // }
+}
+
 // Memoization
 // Time: O(2n)
 // Space: O(2n) + Stack O(n)
 public class Solution2 {
+    
     public int MaxProfit(int[] prices) {
         int[,] dp = new int[prices.Length,2]; //dp states
         ArrayFill(dp, -1); //Time O(2n)
