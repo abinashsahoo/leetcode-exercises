096029d634dbb7b982588dfefb563b88474f0ef7
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-09-12 16:07:31 -0400

    Time: 165 ms (22.22%), Space: 38 MB (11.11%) - LeetHub

--- 948-bag-of-tokens/948-bag-of-tokens.cs
+++ 948-bag-of-tokens/948-bag-of-tokens.cs
@@ -1,5 +1,41 @@
-// Is the tocken list in sorted order? 
+// Is the tocken list in sorted order?
+// Can we play the tokens in any order? ==> Otherwise we can't sort the array!
+
+//Time: O(NLogN) for sorting. The logic itself takes O(N)?
+//Space: O(logN) for quicksort
+
 public class Solution {
+    public int BagOfTokensScore(int[] tokens, int power) 
+    {
+        Array.Sort(tokens);
+        
+        int lo = 0, hi = tokens.Length - 1;
+        int maxScore = 0, score = 0;
+        while(lo <= hi)
+        {
+            if(power >= tokens[lo])
+            {                
+                power -= tokens[lo];
+                lo++;
+                score++;
+            }
+            else if(score >= 1)
+            {
+                power += tokens[hi];
+                hi--;
+                score--;
+            }
+            else
+            {
+                lo++;
+            }
+            maxScore = Math.Max(maxScore, score);
+        }
+        return maxScore;
+    }
+}
+
+public class Solution1 {
     public int BagOfTokensScore(int[] tokens, int power) {
         //Face Up -> Play with smallest valued token -> loose least power
         //Face Down -> play with largest valued token -> gain most power but loosing same score        
@@ -20,7 +56,7 @@ public class Solution {
             }
             
             maxScore = Math.Max(maxScore, score);
-            if (score > 0)
+            if (score > 0) // Do I really need lo <= hi check?
             {
                 power += tokens[hi];
                 hi--;
