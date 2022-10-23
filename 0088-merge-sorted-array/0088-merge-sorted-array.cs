09c44596b5bfec612e74dd3e7c3ac679a456636f
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-10-22 23:22:49 -0400

    Time: 240 ms (37.48%), Space: 41.3 MB (60.43%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cs
@@ -0,0 +1,59 @@
+// Time: O (m + n) : e.g., nums1 {4,5,6}, nums2 = { 1,2,3 }
+public class Solution {
+   
+    public void Merge(int[] nums1, int m, int[] nums2, int n) 
+    {
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int p = m + n - 1;
+        
+        while (p2 >=0)
+        {
+            if(p1 >=0 && nums1[p1] > nums2[p2])
+            {
+                nums1[p--] = nums1[p1--];
+            }
+            else
+            {
+                nums1[p--] = nums2[p2--];
+            }
+        }
+    }
+    
+    public void Merge1(int[] nums1, int m, int[] nums2, int n) 
+    {
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int p3 = m + n - 1;
+        
+        while (p1 >= 0 && p2 >=0)
+        {
+            //NOTE: this is not really needed; 
+            // if(nums1[p1] == nums2[p2])
+            // {
+            //     nums1[p3--] = nums1[p1--];
+            //     nums1[p3--] = nums2[p2--];
+            // }
+            // else 
+            if(nums1[p1] > nums2[p2])
+            {
+                nums1[p3--] = nums1[p1--];
+            }
+            else
+            {
+                nums1[p3--] = nums2[p2--];
+            }
+        }
+        
+        //NOTE: This is also not needed
+        // while (p1 >=0)
+        // {
+        //     nums1[p3--] = nums1[p1--];
+        // }
+        
+        while (p2 >=0)
+        {
+            nums1[p3--] = nums2[p2--];
+        }
+    }
+}
