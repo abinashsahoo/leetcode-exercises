21b8963027f895de64d300a1a82058a9805f3e50
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-10-23 01:33:52 -0400

    Time: 194 ms (35.27%), Space: 39.7 MB (54.77%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cs
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cs
@@ -1,3 +1,46 @@
+// Time: O(log(min(m,n)))
+public class Solution {
+    public double FindMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n1 = nums1.Length;
+        int n2 = nums2.Length;
+        if (n2 < n1) 
+            return FindMedianSortedArrays(nums2, nums1); // Make sure num1 is the shorter one.
+        
+        int lo = 0, hi = n1; // min and max number of elements you can choose from A1
+        while (lo <= hi) 
+        {
+            int cut1 = (lo + hi) >> 1;
+            int cut2 = (n1 + n2 + 1)/2 - cut1;
+
+            int left1 = (cut1 == 0) ? int.MinValue : nums1[cut1 - 1];
+            int left2 = (cut2 == 0) ? int.MinValue : nums2[cut2 - 1];
+            int right1 = (cut1 == n1) ? int.MaxValue : nums1[cut1];
+            int right2 = (cut2 == n2) ? int.MaxValue : nums2[cut2];
+            
+            if(left1 <= right2 && left2 <= right1) //Found the right cut
+            {
+                if((n1 + n2) % 2 == 0)
+                {
+                    return (Math.Max(left1, left2) + Math.Min(right1,right2))/2.0;
+                } 
+                else
+                {
+                    return Math.Max(left1, left2);                    
+                }
+            }
+            else if(left1 > right2)
+            {
+                hi = cut1 - 1;// A1's lower half too big; need to move cut1 left.
+            }
+            else
+            {
+                lo = cut1 + 1;// move cut1 right)
+            }
+        }
+        return 0;
+    }
+}
+
 // Time: O (m + n) : e.g., nums1 {4,5,6}, nums2 = { 1,2,3 }
 public class Solution2 {
     public double FindMedianSortedArrays(int[] nums1, int[] nums2) {
@@ -35,7 +78,7 @@ public class Solution2 {
     }
 }
 //Time: (m + n)log(m + n)
-public class Solution {
+public class Solution1 {
     public double FindMedianSortedArrays(int[] nums1, int[] nums2) {
         int[] union = nums1.Concat(nums2).ToArray();
         Array.Sort(union);
