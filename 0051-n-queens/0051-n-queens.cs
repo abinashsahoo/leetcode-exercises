6d69858f7568b7f9a47d1347f1c84b84b66e4146
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-10-26 22:59:39 -0400

    Time: 244 ms (51.91%), Space: 46.5 MB (56.11%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cs
@@ -0,0 +1,81 @@
+public class Solution {
+    public IList<IList<string>> SolveNQueens(int n) {
+        var result = new List<IList<string>>();
+        var colSet = new HashSet<int>();
+        var diagSet = new HashSet<int>();//r - c
+        var antiDiagSet = new HashSet<int>();//r + c
+        var board = new char?[n, n];
+        
+        void Backtrack(int row)
+        {
+            if (row == n)
+            {                
+                result.Add(GetResult(board).ToList());
+                return;
+            }
+            
+            for (int col = 0; col < n; col++)
+            {
+                if (CanPlace(row, col))
+                {
+                    SetState(row, col);
+                    Backtrack(row + 1);
+                    ResetState(row, col);
+                }
+            }
+        }
+               
+        bool CanPlace (int row, int col) => 
+            (!colSet.Contains(col) && !diagSet.Contains(row - col) && !antiDiagSet.Contains(row + col));
+        
+        void SetState(int row, int col)
+        {
+            diagSet.Add(row - col);
+            antiDiagSet.Add(row + col);
+            colSet.Add(col);
+            board[row, col] = 'Q';
+        }
+        
+        void ResetState(int row, int col)
+        {
+            colSet.Remove(col);
+            diagSet.Remove(row - col);
+            antiDiagSet.Remove(row + col);
+            board[row, col] = null; 
+        }
+        
+        Backtrack(0);
+        return result;
+    }
+    
+    IEnumerable<string> GetResult(char?[,] board)
+    {
+        for (int row = 0; row < board.GetLength(0); row++)
+        {
+            yield return string.Join(",", new string(GetRow().ToArray()));//ToArray !Important
+            IEnumerable<char> GetRow()
+            {
+                for (int col = 0; col < board.GetLength(1); col++)
+                {
+                    yield return board[row, col] ?? '.';
+                }
+            }
+        }
+    }
+                
+//     IEnumerable<string> GetResult(char?[,] board)
+//     {
+//         for (int row = 0; row < board.GetLength(0); row++)
+//         {                  
+//             yield return string.Join(",", new string(GetRow(board, row).ToArray()));
+//         }
+//     }
+    
+//     IEnumerable<char> GetRow(char?[,] board, int row)
+//     {
+//         for (int col = 0; col < board.GetLength(1); col++)
+//         {
+//             yield return board[row, col] ?? '.';
+//         }
+//     }
+}
