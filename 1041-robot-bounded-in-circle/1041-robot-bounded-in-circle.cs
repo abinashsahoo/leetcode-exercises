51bcf8dc21dc4c263b40bc8a478353461dd4b39c
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-10-29 10:19:16 -0400

    Time: 95 ms (82.24%), Space: 36.7 MB (20.56%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cs
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cs
@@ -1,4 +1,4 @@
-public class Solution {
+public class Solution1 {
     public bool IsRobotBounded(string instructions) {
         int x = 0, y = 0;
         int dx = 0, dy = 1;//delta/step -> North facing
@@ -29,4 +29,76 @@ public class Solution {
         }
         return (x == 0 && y == 0) || !(dx == 0 && dy == 1);//Origin or not north
     }
+}
+
+public class Solution {
+    enum Direction {
+        NORTH,
+        SOUTH,
+        WEST,
+        EAST
+    }
+    
+    public bool IsRobotBounded(string instructions) {
+        // TODO validation if no constrains
+        int x = 0, y = 0;
+        Direction direction = Direction.NORTH;
+        
+        foreach (char instruction in instructions) {
+            if (instruction == 'G') {
+                switch (direction) {
+                    case Direction.NORTH:
+                        y++;
+                        break;
+                    case Direction.SOUTH:
+                        y--;
+                        break;
+                    case Direction.WEST:
+                        x--;
+                        break;
+                    case Direction.EAST:
+                        x++;
+                        break;
+                }
+            }
+            else if (instruction == 'L') 
+            {
+                switch (direction) {
+                    case Direction.NORTH:
+                        direction = Direction.WEST;
+                        break;
+                    case Direction.SOUTH:
+                        direction = Direction.EAST;
+                        break;
+                    case Direction.WEST:
+                        direction = Direction.SOUTH;
+                        break;
+                    case Direction.EAST:
+                        direction = Direction.NORTH;
+                        break;
+                }
+            } else if (instruction == 'R') {
+                switch (direction) {
+                    case Direction.NORTH:
+                        direction = Direction.EAST;
+                        break;
+                    case Direction.SOUTH:
+                        direction = Direction.WEST;
+                        break;
+                    case Direction.WEST:
+                        direction = Direction.NORTH;
+                        break;
+                    case Direction.EAST:
+                        direction = Direction.SOUTH;
+                        break;
+                }
+            }
+        }
+        
+        if (x == 0 && y == 0) { return true; }
+        if (direction == Direction.NORTH) { return false; }
+        
+        return true;
+     }
+
 }
