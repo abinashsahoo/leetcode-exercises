0671fc674f73c484b0be10ed7da9c3251e84031a
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-10-29 14:48:55 -0400

    Time: 300 ms (26.63%), Space: 47.5 MB (23.91%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cs
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cs
@@ -1,4 +1,89 @@
+//What's the value of k? What's the Max length of words? What's the size of the array?
+// no new unique words will be added later?
+//uppercase or lowercase?
+
+//MinHeap
+// O (NlogK)= k ≤ N; O(N)+O(Nlogk)+O(klogk) = O(Nlogk)
+//O(N)
 public class Solution {
+    public IList<string> TopKFrequent(string[] words, int k) {
+        var wordFrequency = words.GroupBy(w => w)
+                            .Select(g => new { Word = g.Key, Priority = (g.Count(), g.Key) });
+        //var pq = new PriorityQueue<string,(int, string)>(new FrequencyComparer());
+        var pq = new PriorityQueue<string,(int, string)>
+            (
+                Comparer<(int, string)>.Create((a, b) =>
+                      a.Item1 == b.Item1 ? b.Item2.CompareTo(a.Item2) : a.Item1.CompareTo(b.Item1))
+            );
+        foreach (var wf in wordFrequency)
+        {
+            pq.Enqueue(wf.Word, wf.Priority);
+            if (pq.Count > k)
+            {
+                pq.Dequeue();
+            }
+        }        
+        
+        var result = new List<string>();
+        while (pq.Count > 0)
+        {
+            result.Add(pq.Dequeue());
+        }
+        result.Reverse();
+        return result;
+    }
+    
+    // class FrequencyComparer : IComparer<(int, string)>
+    // {
+    //     public int Compare((int, string) x, (int,string) y)
+    //     {
+    //         if (x.Item1 == y.Item1)
+    //         {
+    //             return y.Item2.CompareTo(x.Item2);//NOTE: this is reversed
+    //         }
+    //         return x.Item1.CompareTo(y.Item1);
+    //     }
+    // }
+}
+
+//MaxHeap
+//when the given input is off-line, i.e., no new unique words will be added later
+//Time: O(N + klogN)
+//Space: O(N)
+public class SolutionMaxHeap {
+    public IList<string> TopKFrequent(string[] words, int k) {
+        var wordFrequency = words.GroupBy(w => w)
+                            .Select(g => (g.Key, (g.Count(), g.Key)));//ValueTuple of (Element, Priority)
+        
+        //Constructs the heap using a heapify operation, which is generally faster than 
+        //enqueuing individual elements sequentially. O(N)
+        var pq = new PriorityQueue<string,(int, string)>(wordFrequency, new FrequencyComparer());
+        var result = new List<string>();
+        for (int i = 0; i < k; i++)
+        {
+            result.Add(pq.Dequeue());
+        }
+        return result;
+    }
+    
+    class FrequencyComparer : IComparer<(int, string)>
+    {
+        public int Compare((int, string) x, (int,string) y)
+        {
+            if (x.Item1 == y.Item1)
+            {
+                return x.Item2.CompareTo(y.Item2);
+            }
+            //return y.CompareTo(x);
+            //OR
+            return y.Item1.CompareTo(x.Item1);
+        }
+    }
+}
+
+//Time: O(NlogN)
+//Space: O(N)
+public class SolutionLinq {
     public IList<string> TopKFrequent(string[] words, int k) {
         return words.GroupBy(w => w)
             .OrderByDescending(g => g.Count()).ThenBy(i => i.Key)
