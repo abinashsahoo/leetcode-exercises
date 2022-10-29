ad8fed0ed0286106847148025038357cd682403a
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-10-29 11:07:41 -0400

    Time: 298 ms (28.86%), Space: 47.8 MB (17.73%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cs
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cs
@@ -1,5 +1,16 @@
 public class Solution {
     public IList<string> TopKFrequent(string[] words, int k) {
+        return words.GroupBy(w => w)
+            .Select(g => new { Frequency = g.Count(), Word = g.Key })//g.Key , it's better that g.First()
+            .OrderByDescending(i => i.Frequency).ThenBy(i => i.Word)
+            .Select(i => i.Word)
+            .Take(k)
+            .ToList();
+    }
+}
+
+public class Solution0 {
+    public IList<string> TopKFrequent(string[] words, int k) {
         return words.GroupBy(w => w).OrderByDescending(g => g.Count())
             .Select(g => new { Frequency = g.Count(), Word = g.First() })
             .OrderByDescending(i => i.Frequency).ThenBy(i => i.Word)
