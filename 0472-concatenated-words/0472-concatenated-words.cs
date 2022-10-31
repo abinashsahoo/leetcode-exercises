8b3ed57bfcbdcd211f79a7ce9a33dc41cf898d72
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-10-31 16:36:11 -0400

    Time: 525 ms (32.67%), Space: 51.4 MB (50.49%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.cs
+++ 0472-concatenated-words/0472-concatenated-words.cs
@@ -45,11 +45,53 @@ public class Solution1 {
     }
 }
 
-public class Solution {
-    public IList<string> FindAllConcatenatedWordsInADict(string[] words) {
+//https://leetcode.com/problems/concatenated-words/discuss/541520/Java-DFS-%2B-Memoization-Clean-code
+public class Solution 
+{
+    public IList<string> FindAllConcatenatedWordsInADict(string[] words)
+    {
+        var result = new List<string>();
+        var wordSet = new HashSet<String>(words);
+        var cache = new Dictionary<string, bool>();
+        foreach(var word in words) 
+        {
+            if (dfs(word, wordSet, cache))
+                result.Add(word);
+        }
+            
+        return result;
+    }
+    
+    bool dfs(String word, HashSet<String> wordSet, Dictionary<string, bool> cache) 
+    {
+        if (cache.ContainsKey(word)) 
+            return cache[word];
+            
+        for (int i = 1; i < word.Length; i++) 
+        {
+            if (wordSet.Contains(word.Substring(0, i))) 
+            {
+                string suffix = word.Substring(i);
+                if (wordSet.Contains(suffix) || dfs(suffix, wordSet, cache)) 
+                {
+                    cache.Add(word, true);
+                    return true;
+                }
+            }
+        }
+        cache.Add(word, false);
+        return false;
+    }
+}
+
+public class Solutionrr {
+    
+    public IList<string> FindAllConcatenatedWordsInADict(string[] words) 
+    {
         var set = new HashSet<String>(words);
         var result = new List<string>();
-        foreach(var word in words){
+        foreach(var word in words)
+        {
             var memo = new bool?[word.Length];
             set.Remove(word);
             if(helper(word, set, 0, memo))
@@ -61,7 +103,25 @@ public class Solution {
         return result;
     }
     
-    private bool helper(String word, HashSet<String> set, int startIndex, bool?[] memo)
+//     public IList<string> FindAllConcatenatedWordsInADict_Works(string[] words) {
+//         var set = new HashSet<String>();
+//         var result = new List<string>();
+//         Array.Sort(words, (a,b) => a.Length - b.Length);//Length wise sorted        
+
+//         foreach(var word in words)
+//         {
+//             var memo = new bool?[word.Length];
+//             //set.Remove(word);
+//             if(helper(word, set, 0, memo))
+//             {
+//                 result.Add(word);
+//             }
+//             set.Add(word);
+//         }
+//         return result;
+//     }
+    
+    private bool helper(string word, HashSet<string> set, int startIndex, bool?[] memo)
     {
         if(startIndex == word.Length)
         {
