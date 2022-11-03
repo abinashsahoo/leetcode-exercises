1653ae63284bc35f17e777d65a893edd8c3b82ca
Abinash Sahoo <abinash.sahoo@gmail.com>
2022-11-02 20:29:43 -0400

    Time: 307 ms (48.73%), Space: 49.1 MB (36.56%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cs
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cs
@@ -1,6 +1,29 @@
 //What if the item is 0? [-1,1,0,-3,3]
 public class Solution {
     public int[] ProductExceptSelf(int[] nums) {
+        int[] result = new int[nums.Length];
+        
+        result[0] = 1;
+        for (int i = 1; i < nums.Length; i++)
+        {
+            result[i] = result[i - 1] * nums[i - 1];//Store the left products
+        }
+        
+        int right = 1;
+        for (int i = nums.Length - 2; i >= 0; i--)
+        {
+            right = right * nums[i + 1];
+            result[i] = result[i] * right;//multiply with right product
+        }        
+        
+        return result;
+    }
+}
+
+//Time O(3N)
+//Space: O(2N)
+public class Solution1 {
+    public int[] ProductExceptSelf(int[] nums) {
         int[] left = new int[nums.Length];
         int[] right = new int[nums.Length];
         
